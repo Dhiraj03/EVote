cce0aa9034ebc1e85d903e2e70e248ccce0be6ea
Dhiraj <dhirajlokesh@gmail.com>
2020-07-24 20:23:31 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Vote',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('E-Vote'),
+      ),
+      body: Container(
+        child: const Center(
+          child:  Text('Welcome to E-Vote'),
+        ),
+      ),
+    );
+  }
+}
