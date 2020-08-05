f39c735057dcff9d3571454dd94df09b69ded8ab
Harshal Dhake <55345366+hmd0404-nitk-eee@users.noreply.github.com>
2020-08-05 10:34:23 +0530

    Implemented Admin Bloc
    Please after cloning place the files in the location ui/bloc/admin_bloc

---
+++ lib/features/ui/bloc/admin_bloc/admin_bloc.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:bloc/bloc.dart';
+import 'package:e_vote/backend/Election.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'admin_event.dart';
+part 'admin_state.dart';
+
+class AdminBloc extends Bloc<AdminEvent, AdminState> {
+  AdminBloc() : super();
+
+  @override
+  AdminState get initialState => AdminInitial();
+
+  final Election _election = Election();
+
+  @override
+  Stream<AdminState> mapEventToState(
+    AdminEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+    if (event is AddCandidateClicked) {
+      yield ProcessingState();
+      await _election.addCandidate(
+          event.nameOfCandidate, event.proposal, event.privateKey);
+      yield AddCandidateOrVoter();
+    }
+    if (event is AddVoterClicked) {
+      yield ProcessingState();
+      await _election.addVoter(event.voterAddress, event.privateKey);
+      yield AddCandidateOrVoter();
+    }
+    if (event is HandleElectionStatusClicked) {
+      if (event.status == 'START') {
+        yield ProcessingState();
+        await _election.startElection(event.privateKey);
+        yield HandleElectionStatus(event.status);
+      } else if (event.status == 'STOP') {
+        yield ProcessingState();
+        await _election.endElection(event.privateKey);
+        yield HandleElectionStatus(event.status);
+      }
+    }
+  }
+}
