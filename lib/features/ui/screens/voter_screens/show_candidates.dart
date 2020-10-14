93c897062622c352ee8dac2baea277c4172aa85b
Dhiraj <dhirajlokesh@gmail.com>
2020-10-14 11:42:52 +0530

    ShowCandidates Screen built

--- lib/features/ui/screens/voter_screens/show_candidates.dart
+++ lib/features/ui/screens/voter_screens/show_candidates.dart
@@ -1,6 +1,9 @@
+import 'package:e_vote/features/auth/presentation/bloc/auth_bloc/auth_barrel_bloc.dart';
+import 'package:e_vote/features/auth/presentation/bloc/auth_bloc/auth_events.dart';
 import 'package:e_vote/features/ui/bloc/voter_bloc/voter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class ShowCandidates extends StatefulWidget {
   @override
@@ -8,7 +11,12 @@ class ShowCandidates extends StatefulWidget {
 }
 
 class _ShowCandidatesState extends State<ShowCandidates> {
-  VoterBloc voterBloc = VoterBloc();
+  VoterBloc voterBloc;
+  @override
+  void initState() {
+    voterBloc = VoterBloc();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,28 +24,220 @@ class _ShowCandidatesState extends State<ShowCandidates> {
       create: (_) => voterBloc..add(DisplayCandidates()),
       child: Container(
         child: Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.refresh, color: Colors.black,),
-                onPressed: () {
-                  voterBloc.add(DisplayCandidates());
-                }),
-            IconButton(
-            icon: Icon(
-              Icons.refresh,
-              color: Colors.black,
+          appBar: AppBar(
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(
+                    Icons.refresh,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    voterBloc.add(DisplayCandidates());
+                  }),
+              IconButton(
+                  icon: Icon(
+                    MaterialCommunityIcons.logout,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    BlocProvider.of<AuthBloc>(context)..add(LoggedOut());
+                  }),
+            ],
+            centerTitle: true,
+            title: Text(
+              'Candidates',
+              style: TextStyle(color: Colors.black87),
             ),
-            onPressed: () {
-              voterBloc.add((DisplayCandidates()));
-            }),
-          ],
-          centerTitle: true,
-          title: Text(
-            'Candidates',
-            style: TextStyle(color: Colors.black87),
           ),
-        ),
+          body: BlocConsumer<VoterBloc, VoterState>(
+              bloc: voterBloc..add(DisplayCandidates()),
+              buildWhen: (previous, current) {
+                if (current is VoterError ||
+                    current is Loading ||
+                    current is ElectionTxHash) {
+                  print('lol');
+                  return false;
+                }
+                return true;
+              },
+              builder: (BuildContext context, VoterState state) {
+                if (state is CandidatesList) {
+                  return ListView.builder(
+                      padding: EdgeInsets.only(left: 10, top: 10, right: 10),
+                      itemCount: state.candidates.length,
+                      itemBuilder: (BuildContext context, int i) {
+                        print(state.candidates.length);
+                        return Card(
+                          color: Color(0xFFFAFAFA),
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                  color: Color(0xFFDCDCDC), width: 0.5),
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        'Candidate ID:   ',
+                                        style: TextStyle(
+                                            color: Color(0xff2F2F2F),
+                                            fontSize: 19,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Flexible(
+                                        child: Text(
+                                            state.candidates[i].id.toString(),
+                                            style: TextStyle(
+                                                color: Color(0xff495057),
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.w400)),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text('Name:               ',
+                                          style: TextStyle(
+                                              color: Color(0xff2F2F2F),
+                                              fontSize: 19,
+                                              fontWeight: FontWeight.w600)),
+                                      Flexible(
+                                        child: Text(
+                                            state.candidates[i].name.toString(),
+                                            style: TextStyle(
+                                                color: Color(0xff2F2F2F),
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.w400)),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text("Proposal:          ",
+                                          style: TextStyle(
+                                              color: Color(0xff2F2F2F),
+                                              fontSize: 19,
+                                              fontWeight: FontWeight.w600)),
+                                      Flexible(
+                                        child: Text(
+                                            state.candidates[i].proposal
+                                                .toString(),
+                                            style: TextStyle(
+                                                color: Color(0xff2F2F2F),
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.w400)),
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Center(
+                                    child: RaisedButton(
+                                      elevation: 1,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5)
+                                      ),
+                                      color: Theme.of(context).primaryColor,
+                                        child: Text(
+                                          'VOTE',
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontSize: 20),
+                                        ),
+                                        onPressed: () {}),
+                                  )
+                                ]),
+                          ),
+                        );
+                      });
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      valueColor: new AlwaysStoppedAnimation<Color>(
+                          Theme.of(context).primaryColor),
+                    ),
+                  );
+                }
+              },
+              listener: (BuildContext context, VoterState state) {
+                if (state is VoterError) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.red[700],
+                      content: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(Icons.error),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              state.errorMessage.message,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ])));
+                } else if (state is ElectionTxHash) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.green,
+                      content: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(
+                              Icons.check,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Flexible(
+                              child: Text(
+                                'TxHash:  ' + state.txHash,
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.center,
+                                maxLines: 3,
+                              ),
+                            ),
+                          ])));
+                } else if (state is Loading) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Theme.of(context).primaryColorDark,
+                      content: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              height: 35,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 2,
+                                valueColor: new AlwaysStoppedAnimation<Color>(
+                                    Colors.white),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Flexible(
+                              child: Text(
+                                'Loading',
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.center,
+                                maxLines: 3,
+                              ),
+                            ),
+                          ])));
+                }
+              }),
         ),
       ),
     );
